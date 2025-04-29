f2709d1a5c27ad995c834078b8ba4fdb119ec698
Deep Bindal <deepbindal36@gmail.com>
2025-04-29 14:04:31 +0530

    bug fix

--- app/api/webhook/clerk/route.js
+++ app/api/webhook/clerk/route.js
@@ -59,7 +59,6 @@ export async function POST(req) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username,
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
@@ -84,7 +83,6 @@ export async function POST(req) {
     const user = {
       firstName: first_name,
       lastName: last_name,
-      username: username,
       photo: image_url,
     }
 
